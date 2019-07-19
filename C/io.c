fe9bb36128834ea5229b3e528305e5f62eac48ae
Wihan Uys <wihan.uys@gmail.com>
2019-07-19 12:49:05 +0200

    printf formatting

--- C/io.c
+++ C/io.c
@@ -18,8 +18,8 @@ int main(int argc, char const *argv[])
 %6d print as decimal integer, at least 6 characters wide
 %f print as floating point
 %6f print as floating point, at least 6 characters wide
-%. 2f print as floating point, 2 characters after decimal point
-%6. 2f print as floating point, at least 6 wide and 2 after decimal point
+%.2f print as floating point, 2 characters after decimal point
+%6.2f print as floating point, at least 6 wide and 2 after decimal point
 */
 
 
